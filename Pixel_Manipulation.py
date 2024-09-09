45c08d7e81317477706793bd6a5ee23d30d9a15c
tech-defence <chand.4kk@gmail.com>
2024-09-09 22:29:30 +0530

    Pixel-Manipulation_Main.py

--- Pixel_Manipulation.py
+++ Pixel_Manipulation.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import os
-from tast2_art import logo,greet_by # Import the logo and created_by information
+from Code_Art import logo,greet_by # Import the logo and created_by information
 
 # ANSI escape codes for colors
 GREEN = "\033[92m"
